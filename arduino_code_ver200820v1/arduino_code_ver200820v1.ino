7642d639f112c59cdcdb2c55fe586bdf86e5697f
Mark Kalda <mark.kalda94@gmail.com>
2020-08-22 16:04:05 +0300

    Lisatud kommentaarid

--- arduino_code_ver200820v1/arduino_code_ver200820v1.ino
+++ arduino_code_ver200820v1/arduino_code_ver200820v1.ino
@@ -101,7 +101,11 @@ void loop() {
 }
 
 
-void read_rc() {
+/*
+ See funktsioon loeb andmed ressiiverist, 
+ muudab nende väärtust 0 ... 1000ni ja kirjutab neid massiivi ch[] 
+*/
+void read_rc() { 
   while (ppmReader.get(count) != 0) {
     ch[count] = map(ppmReader.get(count), 1000, 2000, 0, 1000);
     count++;
@@ -109,7 +113,16 @@ void read_rc() {
   count = 0;
 }
 
-void pushBreak(int channel4) {
+/*
+ See funktsioon võtab andmed ch massiivi 4 elemendist 
+ ja seejärel kontrollib. Kui väärtus on rohkem, kui 10
+ siis aktuaatori draivi DIR pinnile saadetakse LOW signaal
+ ja aktuaatori draivi PWM pinnile saadetakse HIGH signaal.
+ Kui väärtus on vöhem, kui 10 siis aktuaatori draivi 
+ DIR pinnile saadetakse HIGH signaalja aktuaatori draivi 
+ PWM pinnile saadetakse HIGH signaal.
+*/
+void pushBreak(int channel4) { 
   if (channel4 > 10) {
     digitalWrite(breakActuatorDirection, LOW);
     digitalWrite(breakActuatorPwm, HIGH);
@@ -120,6 +133,15 @@ void pushBreak(int channel4) {
   }
 }
 
+/*
+ See funktsioon võtab andmed ch massiivi 0 elemendist
+ ja seejärel kontrollib. Kui väärtus on vähem, kui 485
+ siis samm mootori draivi DIR pinnile saadetakse HIGH signaal
+ ja siis tsüklis iga 80 ms saadetakse pulse (ehk HIGH ja LOW 
+ signaalid üks teise järel) kuni tsükkel jõuab 10000ni. Kui
+ väärtus on rohkem, kui 515 siis samm mootori draivi DIR 
+ pinnile saadetakse LOW signaal ja ülejöönud on sama.
+*/
 void turn_wheels(int channel0) {
   if (channel0 < 485) {
     digitalWrite(turningStepperDirection, HIGH);
@@ -152,6 +174,21 @@ void turn_wheels(int channel0) {
 //}
 }
 
+/*
+ See funktsioon võtab andmed ch massiivi 2 elemendist
+ ja seejärel kontrollib. Kui väärtus on rohkem kui 515
+ siis väärtus muudetakse diaposoonist 515 ... 1000 
+ diaposoonini 45 ... 214, seejärel muudetud väärtus 
+ saadetakse signaalina läbi arduino analoog pinni 
+ kelly kontrollerile ja samal ajal saadetakse ka HIGH 
+ signaal kellysse mootori suuna muutmiseks. Kui väärtus
+ on vähem kui 485 siis toimuvad umbes sama protsessid, 
+ vaid HIGH asemeel mootori suuna muutmiseks saadetakse LOW 
+ signaal. Kui signaal on vahemikus 485 .. 515 siis saadetakse
+ väärtus 45 kellysse > mootori, see vool laseb mootoril olla
+ töötavas seisundis. Kui mingi hetk mootor kaob selle voolu, siis
+ peab tegema terve süsteemi restart.
+ */
 void outputGasSignal(int channel2) {
   if (channel2 > 515) {
     mappedOutputValue = map(channel2, 515, 1000, 45, 214);
@@ -169,6 +206,14 @@ void outputGasSignal(int channel2) {
 
 }
 
+/*
+See funktsioon saadab encoderi CS ja CLK pinnidele 
+HIGH ja LOW signaalid mingis jörjekorras ( seda ma täpselt ei 
+oska seletada, kood on võetud internetist, peaks lugema kuidas
+enkooder töötab) ja siis enkooderist tulevad väärtused läbi D0 (data)
+pinni mis programm muudab diapasoonist 0 .. 1020 diapasoonini 0 .. 359 
+ja see väärtus kirjutatakse globaalse muutujasse currentWheelPosition 
+*/
 void encoder_data() {
   digitalWrite(encoderCsPin, HIGH);
   digitalWrite(encoderCsPin, LOW);
@@ -190,6 +235,14 @@ void encoder_data() {
   digitalWrite(encoderClockPin, HIGH);
 }
 
+/*
+See funktsioon võtab andmed ch massiivi 6 elemendist
+ja seejärel kontrollib. Kui väärtus on vähem kui 400
+siis boolean muutujasse rightLightOn muudab true'ks ja 
+seejörel kolm sekundit saadetakse HIGH signaali parempoolsete
+suunatuledele. Kui taimer jõuab kolme sekundini siis boolean rightLightOn
+muudab false'ks ja parempoolsete suunatuledele saadetakse LOW signaal.
+*/
 void rightTurningLights (int channel6) {
   if (channel6 < 400 ) {
     previousMillisRight = currentMillisRight;
@@ -203,6 +256,14 @@ void rightTurningLights (int channel6) {
   }
 }
 
+/*
+See funktsioon võtab andmed ch massiivi 6 elemendist
+ja seejärel kontrollib. Kui väärtus on rohkem kui 600
+siis boolean muutujasse leftLightOn muudab true'ks ja 
+seejörel kolm sekundit saadetakse HIGH signaali vasakpoolsete
+suunatuledele. Kui taimer jõuab kolme sekundini siis boolean leftLightOn
+muudab false'ks ja vasakpoolsete suunatuledele saadetakse LOW signaal.
+*/
 void leftTurningLights (int channel6) {
   if (channel6 > 600 ) {
     previousMillisLeft = currentMillisLeft;
@@ -217,6 +278,11 @@ void leftTurningLights (int channel6) {
 
 }
 
+/*See funktsioon võtab andmed ch massiivi 5 elemendist
+ja seejärel kontrollib. Kui väärtus on rohkem kui 988
+siis taga- ja esituled lülitakse sisse saadetes rearFrontLights
+pinnile HIGH signaali. Vastasel juhul saadetakse LOW signaal.
+*/
 void turnFrontAndRearLightsOn(int channel5) {
   if (channel5 > 988) {
     digitalWrite(rearFrontLights, HIGH);
