94ecac450d663ed0af6cb22d8a7c5ee900aed7c2
Fredy Kohv <fredykohv14@gmail.com>
2020-09-08 13:48:15 +0300

    Added ssh script

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/expect
+#!/bin/bash
 set timeout -1
-spawn ssh teamv2lk@10.10.0.232
+ssh teamv2lk@10.10.0.232
 expect "password: "
 send "nvidia"
 cd Desktop/ATV/Praktika_ATV/scripts/nvidia_camera.sh
